ebccce32973436c269a56f09c98e97b71146a909
it 21006852 <87711545+it21006852@users.noreply.github.com>
2021-07-21 14:10:33 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 int main() {
+  int maths,science,total,average;
+
+  printf("Enter maths marks");
+  scanf("%d",&maths);
+
+  printf("Enter science marks");
+  scanf("%d",&science);
+
+  total=maths+science;
+  average=total/2;
+  printf("Average is %d",average);
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,22 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int distance,amount;
+
+  printf("Enter the distance the van has travelled");
+  scanf("%d",&distance);
+
+  if(distance <= 30)
+  {
+    amount = distance * 50;
+    printf("Amount is %d",amount);
+  }
+
+  else
+  {
+    amount = 30 * distance + (distance - 30) * 40;
+    printf("Amount is %d",amount);
+  }
   
   return 0;
 }
